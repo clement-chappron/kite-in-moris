b08f2664f48132e3b91982d0995dcf5554097c59
clement-chappron <clement.chappron@gmail.com>
2024-08-28 17:14:36 +0200

    modifictaion de la table et ajout des validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :schools
+  has_many :shops
+  has_many :blog_pages
+  has_many :reviews
+  has_one_attached :profile_picture
+
+  validates :first_name, :last_name, :email, :address, presence: true
 end
---
+++ db/migrate/20240828145834_add_column_to_users.rb
@@ -0,0 +1,8 @@
+class AddColumnToUsers < ActiveRecord::Migration[7.1]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :address, :string
+    add_column :users, :is_manager, :boolean
+  end
+end
