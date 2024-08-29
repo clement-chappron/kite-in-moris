3b356e41b25593279cb1c24cbe74539a67515208
clement-chappron <clement.chappron@gmail.com>
2024-08-29 18:57:04 +0200

    amelioration de la methode update_profile_picture

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -8,22 +8,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def update_profile_picture
     @user = current_user
-    if params[:user] && params[:user][:profile_picture].present?
-      if @user.update(profile_picture: params[:user][:profile_picture])
-        respond_to do |format|
-          format.html { redirect_to edit_user_registration_path, notice: 'Photo de profil mise à jour avec succès.' }
-          format.json { render json: { profile_picture_url: @user.profile_picture.url } }
-        end
-      else
-        respond_to do |format|
-          format.html { render :edit }
-          format.json { render json: @user.errors, status: :unprocessable_entity }
-        end
+    if @user.update(profile_picture_params)
+      respond_to do |format|
+        format.html { redirect_to edit_user_registration_path, notice: 'Photo de profil mise à jour avec succès.' }
+        format.json { render json: { profile_picture_url: url_for(@user.profile_picture) } }
       end
     else
       respond_to do |format|
-        format.html { redirect_to edit_user_registration_path, alert: 'Aucune photo de profil sélectionnée.' }
-        format.json { render json: { error: 'Aucune photo de profil sélectionnée.' }, status: :unprocessable_entity }
+        format.html { redirect_to edit_user_registration_path, alert: 'Échec de la mise à jour de la photo de profil.' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -62,7 +55,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
    protected
-  #
+
+  def profile_picture_params
+    params.require(:user).permit(:profile_picture)
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :address, :profile_picture])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address, :profile_picture])
