117f6221899ef73a5c53c77ef48e3e3bd514b12f
clement-chappron <clement.chappron@gmail.com>
2024-08-31 13:38:33 +0200

    modification du desin de /users/profile

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,16 +10,41 @@ class Users::RegistrationsController < Devise::RegistrationsController
     @user = current_user
     if @user.update(profile_picture_params)
       respond_to do |format|
-        format.html { redirect_to edit_user_registration_path, notice: 'Photo de profil mise à jour avec succès.' }
+        format.html { redirect_to edit_user_registration_path, notice: 'Profile updated successfully' }
         format.json { render json: { profile_picture_url: url_for(@user.profile_picture) } }
       end
     else
       respond_to do |format|
-        format.html { redirect_to edit_user_registration_path, alert: 'Échec de la mise à jour de la photo de profil.' }
+        format.html { redirect_to edit_user_registration_path, alert: 'Failed to update profile.' }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
+
+  def edit_image_and_bio
+    if user_signed_in?
+      @user = current_user
+    else
+      redirect_to edit_image_and_bio_path
+    end
+  end
+
+  def update_image_and_bio
+    @user = current_user
+    if @user.update(profile_picture_and_bio_params)
+      respond_to do |format|
+        format.html { redirect_to edit_image_and_bio_path, notice: 'Profile updated successfully.' }
+        format.json { render json: { profile_picture_url: url_for(@user.profile_picture) } }
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to edit_image_and_bio_path, alert: 'Failed to update profile.' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
   # GET /resource/sign_up
   # def new
   #   super
@@ -56,8 +81,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
    protected
 
-  def profile_picture_params
-    params.require(:user).permit(:profile_picture)
+  def profile_picture_and_bio_params
+    params.require(:user).permit(:profile_picture, :bio)
   end
 
   def configure_permitted_parameters
