99a35e980c12a4119f49d5da602fb09bc437ddb2
clement chappron <clement.chappron@gmail.com>
2024-09-02 21:54:07 +0200

    amélioration du boutton du menu

--- app/javascript/navbar-button-effect.js
+++ app/javascript/navbar-button-effect.js
@@ -1,7 +1,22 @@
 const menuWrapper = document.querySelector('.menu-wrapper');
 const hamburgerMenu = document.querySelector('.hamburger-menu');
+const hamburgerBg = document.querySelector('.hamburger-bg');
 
 menuWrapper.addEventListener('click', () => {
   hamburgerMenu.classList.toggle('animate');
   }
 );
+hamburgerBg.addEventListener('mouseover', () => {
+  hamburgerBg.classList.add('animate');
+});
+
+hamburgerBg.addEventListener('mouseout', () => {
+  hamburgerBg.classList.remove('animate');
+});
+hamburgerMenu.addEventListener('mouseover', () => {
+  hamburgerBg.classList.add('animate');
+});
+
+hamburgerMenu.addEventListener('mouseout', () => {
+  hamburgerBg.classList.remove('animate');
+});
