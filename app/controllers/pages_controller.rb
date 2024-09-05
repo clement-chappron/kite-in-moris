3e4935ff24321c642f96881eb76d69211bd6c64c
clement chappron <clement.chappron@gmail.com>
2024-09-05 12:35:37 +0200

    filter by location fonctionnel

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,31 @@
+# class PagesController < ApplicationController
+#   skip_before_action :authenticate_user!, only: [ :home, :filter_categories ]
+
+#   def home
+#     @locations = Location.all
+#   end
+
+#   def filter_categories
+#     @category = params[:category]
+
+#     @cards = case @category
+#              when 'all'
+#                [School.all, Shop.all, Spot.all].flatten.sort_by(&:name)
+#              when 'schools'
+#                School.all.sort_by(&:name)
+#              when 'shops'
+#                Shop.all.sort_by(&:name)
+#              when 'spots'
+#                Spot.all.sort_by(&:name)
+#              else
+#               []
+#              end
+
+#     respond_to do |format|
+#       format.html { render partial: "pages/cards", locals: { cards: @cards } }
+#     end
+#   end
+# end
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home, :filter_categories ]
 
@@ -7,6 +35,7 @@ class PagesController < ApplicationController
 
   def filter_categories
     @category = params[:category]
+    @location = params[:location]
 
     @cards = case @category
              when 'all'
@@ -20,6 +49,11 @@ class PagesController < ApplicationController
              else
               []
              end
+
+    # Apply location filter if necessary
+    if @location.present? && @location != 'all'
+      @cards = @cards.select { |card| card.location.name == @location }
+    end
 
     respond_to do |format|
       format.html { render partial: "pages/cards", locals: { cards: @cards } }
--- app/javascript/controllers/categories_filter_controller.js
+++ app/javascript/controllers/categories_filter_controller.js
@@ -1,9 +1,16 @@
 import { Controller } from "@hotwired/stimulus"
+
 export default class extends Controller {
-  static targets = ["cards"]
+  static targets = ["locations", "locationBtn", "cards"]
 
   connect() {
-    fetch(`/filter_categories?category=all`, {
+    this.selectedCategory = 'all';
+    this.selectedLocation = 'all';
+    this.loadCards();
+  }
+
+  loadCards() {
+    fetch(`/filter_categories?category=${this.selectedCategory}&location=${this.selectedLocation}`, {
       headers: {
         "Accept": "text/html"
       }
@@ -11,9 +18,14 @@ export default class extends Controller {
     .then(response => response.text())
     .then(html => {
         this.cardsTarget.innerHTML = html;
-
       })
-    }
+  }
+
+  locations(event) {
+    event.preventDefault();
+    this.locationsTarget.classList.toggle("d-none");
+    this.locationBtnTarget.classList.toggle("border-green");
+  }
 
   filter(event) {
     event.preventDefault();
@@ -23,17 +35,19 @@ export default class extends Controller {
     });
     event.currentTarget.classList.add('active-category');
 
-    const category = event.currentTarget.dataset.category;
+    this.selectedCategory = event.currentTarget.dataset.category;
+    this.loadCards();
+  }
 
-    fetch(`/filter_categories?category=${category}`, {
-      headers: {
-        "Accept": "text/html"
-      }
-    })
-    .then(response => response.text())
-    .then(html => {
-        this.cardsTarget.innerHTML = html;
+  filterByLocation(event) {
+    event.preventDefault();
 
-      })
+    document.querySelectorAll('.location-items').forEach(item => {
+      item.classList.remove('active-location');
+    });
+    event.currentTarget.classList.add('active-location');
+
+    this.selectedLocation = event.currentTarget.dataset.location;
+    this.loadCards();
   }
 }
