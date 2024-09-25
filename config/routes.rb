a8066e815abfb2bc24da2fb745ae0729e1751a52
clement chappron <clement.chappron@gmail.com>
2024-09-25 22:30:08 +0400

    load more fonctionnel sur le shop#show

--- config/routes.rb
+++ config/routes.rb
@@ -39,6 +39,8 @@ Rails.application.routes.draw do
     resources :review_spots, only: [:new, :create]
   end
   get 'load_more', to: 'review_spots#load_more'
+  get 'load_more_shop_reviews', to: 'review_shops#load_more'
+
   get 'search', to: 'pages#search'
 
 end
