264af70a59e21f650a2997474648449b10e14cf5
Eldave DIBALA <eldavedibala@gmail.com>
2024-09-26 19:03:30 +0100

    Creation des shops et schools ok

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -3,22 +3,14 @@ class SchoolsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show]
   before_action :set_school, only: [:edit, :update, :show, :destroy]
   before_action :set_school_step, only: [:create_step_1, :create_step_2, :create_step_3, :create_step_4]
-  before_action :find_school, only: [:create_step_2, :create_step_3, :create_step_4]
-
 
   def show
-    @review_schools = ReviewSchool.all
-    # @review_school = ReviewSchool.new
-
-    @school = School.find(params[:id])
-
     @review_schools = @school.review_schools
     @average_rating = @review_schools.average(:rating).to_f.round(1)
     @total_review_schools = @review_schools.count
-
     @review_school = @school.review_schools.build
     @review_schools = @school.review_schools.limit(5)
-    @more_reviews_exist = @school.review_schools.count > 5
+    @more_reviews_exist = @total_review_schools > 5
   end
 
   def new
@@ -29,6 +21,7 @@ class SchoolsController < ApplicationController
 
   def create_step_1
     @school.assign_attributes(school_params_step_1)
+    @school.user = current_user
     if @school.save
       redirect_to new_school_path(step: 2)
     else
@@ -56,24 +49,22 @@ class SchoolsController < ApplicationController
   end
 
   def create_step_4
+    Rails.logger.info("Paramètres avant la mise à jour : #{school_params_step_4.inspect}")
     if @school.update(school_params_step_4)
-      redirect_to @school, notice: 'Établissement créé avec succès.'
+      redirect_to school_path(@school), notice: 'Établissement créé avec succès.'
     else
+      Rails.logger.error("Erreur lors de la mise à jour : #{@school.errors.full_messages}")
       @step = 4
       render :new
     end
-  end
+  end  
 
   def update
-    @school = School.find(params[:id])
-  
-    # Attacher les nouvelles images sans supprimer les anciennes
     if params[:school][:images]
       @school.images.attach(params[:school][:images])
     end
-  
     if @school.update(school_params)
-      redirect_to @school, notice: 'School was successfully updated.'
+      redirect_to @school, notice: 'L\'école a été mise à jour avec succès.'
     else
       render :edit
     end
@@ -81,9 +72,9 @@ class SchoolsController < ApplicationController
 
   def destroy
     @school.destroy
-    redirect_to schools_path, notice: 'School was successfully deleted.'
+    redirect_to schools_path, notice: 'L\'école a été supprimée avec succès.'
   rescue ActiveRecord::RecordNotFound
-    redirect_to schools_path, alert: 'School not found.'
+    redirect_to schools_path, alert: 'École non trouvée.'
   end
 
   private
@@ -91,15 +82,16 @@ class SchoolsController < ApplicationController
   def set_school
     @school = School.find(params[:id])
   rescue ActiveRecord::RecordNotFound
-    redirect_to schools_path, alert: 'School not found.'
+    redirect_to schools_path, alert: 'École non trouvée.'
   end
 
   def set_school_step
     @school = School.find_or_initialize_by(id: params[:id])
+    @school.user ||= current_user
   end
 
   def school_params_step_1
-    params.require(:school).permit(:name, :address, :phone, :website, :email)
+    params.require(:school).permit(:name, :email, :address, :phone, :website, :description, :rental, :levels, :fee, :location_id, :facebook, :instagram, :latitude, :longitude, :category, images: [])
   end
 
   def school_params_step_2
@@ -115,6 +107,6 @@ class SchoolsController < ApplicationController
   end
 
   def school_params
-    params.require(:school).permit(:name, :email, :address, :phone, :website, :description, :rental, :levels, :fee, :location_id, :facebook, :instagram, :latitude, :longitude, :category, images: [] )
+    params.require(:school).permit(:name, :email, :address, :phone, :website, :description, :rental, :levels, :fee, :location_id, :facebook, :instagram, :latitude, :longitude, :category, images: [])
   end
 end
